9fd9f9654e909180c0e93352f5a8e2b0b28dd068
Meeka <rrgayhart@gmail.com>
2013-11-03 18:19:42 -0700

    Adds integration test framework

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,2 +1,3 @@
 class Chore < ActiveRecord::Base
+  validates :title, presence: true
 end
--- test/models/chore_test.rb
+++ test/models/chore_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class ChoreTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  def test_it_should_not_save_chores_without_titles
+    chore = Chore.new
+    assert !chore.save
+    chore = Chore.new(title: "Wash dishes")
+    assert chore.save
   end
 end
