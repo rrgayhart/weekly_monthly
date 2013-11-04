6166c2cbca7c9416c5db18ad5134681fc3ca28c2
Meeka <rrgayhart@gmail.com>
2013-11-03 17:11:37 -0700

    Initial set up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeeklyMonthly::Application.config.secret_key_base = 'd564f865a0b965096c704efa3517c4771ce2f0ee6969a5d76136bb2dccd0f9ecbfcff5f036653ea321ec95c9e22348be2bd0a51aef6b1f27205b82ee7e25fc53'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WeeklyMonthly::Application.config.session_store :cookie_store, key: '_weekly_monthly_session'
